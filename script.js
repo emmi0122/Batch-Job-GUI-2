00156e154cc0cfde2b05dcba203d983e0e18c0d3
viggo2003 <vignie1217@skola.goteborg.se>
2025-05-07 14:40:27 +0200

    added js functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+// let button = document.getElementById('filter-button'); 
+// button.addEventListener('click',()=> alert("d"));
+
+document.getElementById('filter-button').addEventListener("click", function() {
+    window.location.href = "filter.html";
+})
